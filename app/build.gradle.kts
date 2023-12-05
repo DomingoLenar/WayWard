f459a62d301f2334da0e04980871de3b9e2c55d9
Lestat <2233915@slu.edu.ph>
2023-12-06 00:43:03 +0800

    +changed min sdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     compileSdk = 33
     defaultConfig {
         applicationId = "com.example.myfirstapp"
-        minSdk = 23
+        minSdk = 26
         targetSdk = 33
         versionCode = 1
         versionName = "1.0-${System.getenv("VERSION_SHA")}"
