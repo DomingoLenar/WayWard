72a46fb01947b794ecba17a3402067d5fb2e633a
Lestat <2233915@slu.edu.ph>
2023-12-06 00:58:33 +0800

    +documentation

--- app/src/main/java/com/example/myfirstapp/models/User.java
+++ app/src/main/java/com/example/myfirstapp/models/User.java
@@ -1,6 +1,10 @@
 package com.example.myfirstapp.models;
 
 import java.sql.*;
+
+/**
+ * User model class
+ */
 public class User {
     private String username;
     private String password;
@@ -10,6 +14,12 @@ public class User {
     private String lastName;
     private final DataBase db = new DataBase();
 
+    /**
+     * This constructor takes in three parameters and leaves the details of names as null
+     * @param username
+     * @param password
+     * @param hashed
+     */
     public User(String username, String password, boolean hashed){
         this.username = username;
         if(hashed){
@@ -22,6 +32,15 @@ public class User {
         this.lastName = null;
     }
 
+    /**
+     * Is the complete constructor of the class
+     * @param username
+     * @param password
+     * @param hashed
+     * @param firstName
+     * @param middleName
+     * @param lastName
+     */
     public User(String username, String password, boolean hashed, String firstName, String middleName, String lastName){
         this.username = username;
         this.firstName = firstName;
@@ -34,6 +53,13 @@ public class User {
         }
     }
 
+    //This should be moved to DataBase class in further revisions
+    /**
+     * This method fetches the user from the database using the parameter username
+     * this returns an object of User
+     * @param username
+     * @return User
+     */
     public User fetchUser(String username){
         try{
             Connection conn = db.createConnection();
@@ -58,6 +84,9 @@ public class User {
         return null;
     }
 
+    /**
+     * Inserts the current object of user to the database
+     */
     public void insertCurrentUser(){
         try{
             Connection conn = db.createConnection();
@@ -69,6 +98,10 @@ public class User {
         }
     }
 
+    /**
+     * Checks if the username already exists in the database
+     * @return boolean
+     */
     public boolean isUsernameValid(){
         try {
             Connection conn = db.createConnection();
@@ -81,6 +114,10 @@ public class User {
         }
     }
 
+    /**
+     * Method used to authenticate given password with the stored password in the database
+     * @return boolean
+     */
     public boolean authenticate(){
         User dbUser = fetchUser(this.username);
 
@@ -88,16 +125,29 @@ public class User {
         return this.password.equals(dbUser.getPassword());
     }
 
+    /**
+     * This method hashes the password to provide a level of security in storing the password
+     * @param rawPassword Takes in a string that is a raw unhashed version of the password
+     * @return String
+     */
     private String hashPassword(String rawPassword){
         String hashed="";
 
         return hashed;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getPassword(){
         return this.password;
     }
 
+    /**
+     *
+     * @return
+     */
     public void setPassword(String password, boolean hashed){
         if(!hashed){
             this.password = hashPassword(password);
@@ -106,35 +156,67 @@ public class User {
         }
     }
 
+    /**
+     *
+     * @return
+     */
     public String getUsername(){
         return this.username;
 
     }
 
+    /**
+     *
+     * @return
+     */
     public void setUsername(String username){
         this.username = username;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getFirstName(){
         return this.firstName;
     }
 
+    /**
+     *
+     * @return
+     */
     public void setFirstname(String firstName){
         this.firstName = firstName;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getMiddleName(){
         return this.middleName;
     }
 
+    /**
+     *
+     * @return
+     */
     public void setMiddleName(String middleName){
         this.middleName = middleName;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getLastName(){
         return this.lastName;
     }
 
+    /**
+     *
+     * @return
+     */
     public void setLastName(String lastName){
         this.lastName = lastName;
     }
