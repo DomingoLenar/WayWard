53f315c6292c9d98bed354c87dc8b64316c20c3a
Lestat <2233915@slu.edu.ph>
2023-12-06 08:52:14 +0800

    +setter getter

--- app/src/main/java/com/example/myfirstapp/models/ContactDetails.java
+++ app/src/main/java/com/example/myfirstapp/models/ContactDetails.java
@@ -34,4 +34,36 @@ public class ContactDetails {
             e.printStackTrace();
         }
     }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
