79b1df09d1545b55fda4918f37e701ae3ba2c726
Lou <2233672@slu.edu.ph>
2023-12-06 20:43:38 +0800

    + SigninController.java and connection to SigninActivity.java

---
+++ app/src/main/java/com/example/myfirstapp/controllers/SigninController.java
@@ -0,0 +1,15 @@
+package com.example.myfirstapp.controllers;
+
+import com.example.myfirstapp.views.SigninActivity;
+
+public class SigninController {
+
+    SigninActivity signinActivity;
+
+    public SigninController(SigninActivity signinActivity) {
+        this.signinActivity = signinActivity;
+    }
+
+
+
+}
