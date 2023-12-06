d98647b013e8296da51e0d872fddcff5eb0a668a
Lou <2233672@slu.edu.ph>
2023-12-06 20:41:33 +0800

    + connection to LoginController.java

--- app/src/main/java/com/example/myfirstapp/views/LoginActivity.java
+++ app/src/main/java/com/example/myfirstapp/views/LoginActivity.java
@@ -1,20 +1,32 @@
-package com.example.myfirstapp.view;
+package com.example.myfirstapp.views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.myfirstapp.R;
+import com.example.myfirstapp.controllers.LoginController;
 
 public class LoginActivity extends AppCompatActivity {
+
+    LoginController loginController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginController = new LoginController(this);
+
+        initViews();
+
+    }
+
+    private void initViews() {
 
         TextView loginTitle = findViewById(R.id.loginTitle);
         TextView loginDescription = findViewById(R.id.loginDescription);
@@ -23,5 +35,15 @@ public class LoginActivity extends AppCompatActivity {
         Button loginSignUpButton = findViewById(R.id.loginSignUpButton);
 
         ImageView loginLogo = findViewById(R.id.loginLogo);
+
     }
+
+    public void signIn(View view) {
+        loginController.displaySignInActivity(this);
+    }
+
+    public void signUp(View view) {
+        loginController.displaySignUpActivity(this);
+    }
+
 }
