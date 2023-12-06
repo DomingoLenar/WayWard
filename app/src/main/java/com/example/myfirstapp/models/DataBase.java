0a174d91d956de75a8c3c5bf79da2a1732ad3150
Lestat <2233915@slu.edu.ph>
2023-12-06 08:52:35 +0800

    +moved fetchUser from User to DataBase

--- app/src/main/java/com/example/myfirstapp/models/DataBase.java
+++ app/src/main/java/com/example/myfirstapp/models/DataBase.java
@@ -18,4 +18,36 @@ public class DataBase {
         return null;
     }
 
+    /**
+     * This method fetches the user from the database using the parameter username
+     * this returns an object of User
+     * @param username
+     * @return User
+     */
+    public User fetchUser(String username){
+        try{
+            Connection conn = createConnection();
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery("SELECT * FROM user_details WHERE username = "+username);
+            if(rs.next()){
+                //Gets data from ResultSet and segregates them into respective variables
+                String fetchedUsername = rs.getString(1);
+                String fetchedPassword = rs.getString(2);
+                int fetchedUserID = rs.getInt(3);
+                String fetchedFName = rs.getString(4);
+                String fetchedMName = rs.getString(5);
+                String fetchedLName = rs.getString(6);
+                // constructs and returns a new object of user based on the fetched data
+                return new User(fetchedUsername, fetchedPassword, true,fetchedFName, fetchedMName, fetchedLName);
+            }else{
+                return null;
+            }
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+
 }
--- app/src/main/java/com/example/myfirstapp/models/User.java
+++ app/src/main/java/com/example/myfirstapp/models/User.java
@@ -53,36 +53,6 @@ public class User {
         }
     }
 
-    //This should be moved to DataBase class in further revisions
-    /**
-     * This method fetches the user from the database using the parameter username
-     * this returns an object of User
-     * @param username
-     * @return User
-     */
-    public User fetchUser(String username){
-        try{
-            Connection conn = db.createConnection();
-            Statement st = conn.createStatement();
-            ResultSet rs = st.executeQuery("SELECT * FROM user_details WHERE username = "+username);
-            if(rs.next()){
-                //Gets data from ResultSet and segregates them into respective variables
-                String fetchedUsername = rs.getString(1);
-                String fetchedPassword = rs.getString(2);
-                int fetchedUserID = rs.getInt(3);
-                String fetchedFName = rs.getString(4);
-                String fetchedMName = rs.getString(5);
-                String fetchedLName = rs.getString(6);
-                // constructs and returns a new object of user based on the fetched data
-                return new User(fetchedUsername, fetchedPassword, true,fetchedFName, fetchedMName, fetchedLName);
-            }else{
-                return null;
-            }
-        }catch(SQLException e){
-            e.printStackTrace();
-        }
-        return null;
-    }
 
     /**
      * Inserts the current object of user to the database
@@ -119,7 +89,7 @@ public class User {
      * @return boolean
      */
     public boolean authenticate(){
-        User dbUser = fetchUser(this.username);
+        User dbUser = db.fetchUser(this.username);
 
         //insert logic to compare two objects of user
         return this.password.equals(dbUser.getPassword());
