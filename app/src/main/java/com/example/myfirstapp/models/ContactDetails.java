ee8653ff2c5fa60ba12cdf876f6cc4dcb16d947a
Lestat <2233915@slu.edu.ph>
2023-12-07 23:02:41 +0800

    +methods to update the database, documentation

--- app/src/main/java/com/example/myfirstapp/models/ContactDetails.java
+++ app/src/main/java/com/example/myfirstapp/models/ContactDetails.java
@@ -12,13 +12,14 @@ public class ContactDetails {
     private String phoneNumber;
     private int id = -1;
     private DataBase db = new DataBase();
-    public ContactDetails(String username,  String email){
-        this.username = username;
-        this.email = email;
-        this.address = null;
-        this.phoneNumber = null;
-    }
 
+    /**
+     * Default constructor
+     * @param username  Username of the user to whom these details belong to
+     * @param email Email of the user
+     * @param phoneNumber   Phone number of the user
+     * @param address   Address of the user
+     */
     public ContactDetails(String username, String email, String phoneNumber, String address){
         this.username = username;
         this.email = email;
@@ -26,6 +27,21 @@ public class ContactDetails {
         this.address = address;
     }
 
+    /**
+     * Alternative constructor for when if the phone number and address is not provided
+     * @param username  Username of the user to whom these details belong to
+     * @param email Email of the user
+     */
+    public ContactDetails(String username,  String email){
+        this.username = username;
+        this.email = email;
+        this.address = null;
+        this.phoneNumber = null;
+    }
+
+    /**
+     * Inserts current instance of the object into the database
+     */
     public void insertCurrentCD(){
         try{
             Connection conn = db.createConnection();
@@ -36,37 +52,102 @@ public class ContactDetails {
         }
     }
 
+    /**
+     * Updates the database with the new address
+     * @param newAddress    New address to be used to replace the old one
+     */
+    public void updateAddress(String newAddress){
+        try{
+            Connection conn = db.createConnection();
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery("UPDATE contact_details SET address = '"+newAddress+"' WHERE username = '"+this.username+"'");
+        }catch(SQLException updateAddressException){
+            updateAddressException.printStackTrace();
+        }
+    }
+
+    /**
+     * Updates the database with the new phone number
+     * @param newNumber     New phone number to replace the old one
+     */
+    public void updateNumber(String newNumber){
+        try{
+            Connection conn = db.createConnection();
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery("UPDATE contact_details SET address = '"+newNumber+"' WHERE username = '"+this.username+"'");
+        }catch(SQLException updateNumberException){
+            updateNumberException.printStackTrace();
+        }
+    }
+
+    /**
+     *
+     * @return
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     *
+     * @param username
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getAddress() {
         return address;
     }
 
+    /**
+     *
+     * @param address
+     */
     public void setAddress(String address) {
         this.address = address;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     *
+     * @param email
+     */
     public void setEmail(String email) {
         this.email = email;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getPhoneNumber() {
         return phoneNumber;
     }
 
+    /**
+     *
+     * @param phoneNumber
+     */
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
+
+    /**
+     *
+     * @param id
+     */
     public void setId(int id){
         this.id = id;
     }
