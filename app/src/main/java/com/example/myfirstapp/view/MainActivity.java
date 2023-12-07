f1aa647629f8be54b222a9c44e9299711d9b39d4
Lenar R. Domingo <2233894@slu.edu.ph>
2023-12-08 01:38:52 +0800

    + Start of Main Activity

---
+++ app/src/main/java/com/example/myfirstapp/view/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.myfirstapp.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.Image;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.myfirstapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
