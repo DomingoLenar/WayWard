4639b031838cdfd457ed97f07df46092e3577be4
Lou <2233672@slu.edu.ph>
2023-12-09 09:28:56 +0800

    + minor changes in controllers

--- app/src/main/java/com/example/myfirstapp/controllers/SigninController.java
+++ app/src/main/java/com/example/myfirstapp/controllers/SigninController.java
@@ -1,15 +1,41 @@
 package com.example.myfirstapp.controllers;
 
+import android.content.Intent;
+
+import com.example.myfirstapp.models.User;
+import com.example.myfirstapp.views.MainActivity;
 import com.example.myfirstapp.views.SigninActivity;
 
 public class SigninController {
 
     SigninActivity signinActivity;
+    User userModel;
+
 
     public SigninController(SigninActivity signinActivity) {
         this.signinActivity = signinActivity;
+        userModel = new User(this);
     }
 
 
+    public void displayMainActivity(SigninActivity signinActivity) {
 
+        Intent i = new Intent(signinActivity, MainActivity.class);
+        signinActivity.startActivity(i);
+
+    }
+
+    public void submitAccountDetails(String username, String password) {
+
+        displayMainActivity(signinActivity); // temporary...
+
+        userModel = new User(username, password, false);
+        boolean valid = userModel.authenticate();
+
+        if (valid) {
+            displayMainActivity(signinActivity);
+        } else {
+            // notify user that username || password incorrect
+        }
+    }
 }
--- app/src/main/java/com/example/myfirstapp/controllers/SignupController.java
+++ app/src/main/java/com/example/myfirstapp/controllers/SignupController.java
@@ -1,7 +1,10 @@
 package com.example.myfirstapp.controllers;
 
+import android.content.Intent;
+
 import com.example.myfirstapp.models.ContactDetails;
 import com.example.myfirstapp.models.User;
+import com.example.myfirstapp.views.MainActivity;
 import com.example.myfirstapp.views.SignupActivity;
 
 public class SignupController {
@@ -25,8 +28,26 @@ public class SignupController {
 
     public void displayMainActivity(SignupActivity signupActivity) { // display Home page
 
-//        Intent i = new Intent(signupActivity, MainActivity.class);
-//        signupActivity.startActivity(i);
+        Intent i = new Intent(signupActivity, MainActivity.class);
+        signupActivity.startActivity(i);
 
+    }
+
+    public void submitAccountDetails(String email, String username, String password, String fName, String lName, String phoneNo) {
+
+        displayMainActivity(signupActivity); // temporary...
+
+        userModel = new User(username, password, false, fName, lName);
+//        boolean valid = userModel.insertCurrentUser(); // *returns boolean
+
+        contactDetails = new ContactDetails(username, email, phoneNo);
+        contactDetails.insertCurrentCD(); // *returns boolean?
+
+
+//        if (valid) {
+//            displayMainActivity(signupActivity);
+//        } else {
+//            // notify user what is wrong
+//        }
     }
 }
